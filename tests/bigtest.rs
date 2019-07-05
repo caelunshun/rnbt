d947661a1077626264092f700fa46492143c515c
caelunshun <caelum12321@gmail.com>
2019-07-05 13:48:47 -0600

    Improve extensibility of bigtest

--- tests/bigtest.rs
+++ tests/bigtest.rs
@@ -24,4 +24,14 @@ fn bigtest() {
 
     // Parse NBT
     let root = rnbt::parse(&buf).unwrap();
+
+    // Confirm that values are correct
+    let c = root.compound().unwrap();
+
+    assert_eq!(c.get("intTest").unwrap().int().unwrap().value, 2147483647);
+    assert_eq!(c.get("byteTest").unwrap().byte().unwrap().value, 127);
+    assert_eq!(c.get("doubleTest").unwrap().double().unwrap().value, 0.49312871321823148);
+    assert_eq!(c.get("floatTest").unwrap().float().unwrap().value, 0.49823147058486938);
+    assert_eq!(c.get("longTest").unwrap().long().unwrap().value, 9223372036854775807);
+    assert_eq!(c.get("shortTest").unwrap().short().unwrap().value, 32767);
 }
